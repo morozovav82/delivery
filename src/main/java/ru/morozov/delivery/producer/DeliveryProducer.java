949593845c7338cdf7f3ea8ba71e06320106f80f
morozov <morozov82@mail.ru>
2021-02-14 15:02:48 +0300

    гарантированная отправка сообщений (правка отправки Rollback-сообщений)

--- src/main/java/ru/morozov/delivery/producer/DeliveryProducer.java
+++ src/main/java/ru/morozov/delivery/producer/DeliveryProducer.java
@@ -3,12 +3,14 @@ package ru.morozov.delivery.producer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import ru.morozov.delivery.service.MessageService;
 import ru.morozov.messages.DeliveryRejectedMsg;
 import ru.morozov.messages.DeliveryScheduledMsg;
 
-@Component
+@Service
 @Slf4j
 public class DeliveryProducer {
 
@@ -25,6 +27,7 @@ public class DeliveryProducer {
         messageService.scheduleSentMessage(deliveryScheduledTopic, null, message, DeliveryScheduledMsg.class);
     }
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void sendDeliveryRejectedMessage(DeliveryRejectedMsg message){
         messageService.scheduleSentMessage(deliveryRejectedTopic, null, message, DeliveryRejectedMsg.class);
     }
