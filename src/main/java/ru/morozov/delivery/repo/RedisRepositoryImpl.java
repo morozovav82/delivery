ee28ea13fd9b8444f5de547b5fd73322b73cb5ff
morozov <morozov82@mail.ru>
2021-02-16 12:52:48 +0300

    change Redis KEY

--- src/main/java/ru/morozov/delivery/repo/RedisRepositoryImpl.java
+++ src/main/java/ru/morozov/delivery/repo/RedisRepositoryImpl.java
@@ -12,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class RedisRepositoryImpl implements RedisRepository {
 
-    private static final String KEY = "DELIVERY_IDEMPOTENCE";
+    private static final String KEY = "DELIVERY";
     private final RedisTemplate<String, Object> redisTemplate;
     private HashOperations hashOperations;
 
